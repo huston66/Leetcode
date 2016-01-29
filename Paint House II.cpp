c22b0e7b6de68dc0d738856fa700b20f88b25ab8
huston66 <zhenli@Zhens-MacBook-Pro.local>
2016-01-29 11:19:50 -0800

    coding

---
+++ Paint House II.cpp
@@ -0,0 +1,42 @@
+public class Solution {
+    public int minCostII(vector<vector<int>>costs) {
+        int m=costs.size();
+        if(m==0)
+            return 0;
+
+        int k=costs[0].size();
+        int min1=0;
+        int min2=0;
+        vector<int>dp(k,0);
+
+        for(int i=0;i<m;i++)
+        {
+            int oldmin1=min1;
+            int oldmin2=min2;
+            min1=INT_MAX;
+            min2=INT_MAX;
+            for(int j=0;j<k;j++)
+            {
+                if(dp[j]!=oldmin1||oldmin1==oldmin2){
+                    dp[j]=oldmin1+costs[i][j];
+                }
+                else
+                {
+                    dp[j]=oldmin2+costs[i][j];
+                }
+
+                if(dp[j]<min1)
+                {
+                    min2=min1;
+                    min1=dp[j];
+                }
+                else if(dp[j]<min2)
+                {
+                    min2=dp[j];
+                }
+            }
+        }
+
+        return min1;
+    }
+};
---
+++ Paint House.cpp
@@ -0,0 +1,26 @@
+public class Solution {
+    public int minCost(vector<vector<int>> costs) {
+        int m=costs.size();
+        if(m==0)
+            return 0;
+
+        vector<int>pre(3,0);
+        vector<int>curr(3,0);
+        pre[0]=costs[0][0];
+        pre[1]=costs[0][1];
+        pre[2]=costs[0][2];
+
+        for(int i=1;i<m;i++)
+        {
+            curr[0]=min(pre[1],pre[2])+costs[i][0];
+            curr[1]=min(pre[0],pre[2])+costs[i][1];
+            curr[2]=min(pre[0],pre[1])+costs[i][2];
+
+            pre[0]=curr[0];
+            pre[1]=curr[1];
+            pre[2]=curr[2];
+        }
+
+        return min(pre[0],min(pre[1],pre[2]));
+    }
+};
